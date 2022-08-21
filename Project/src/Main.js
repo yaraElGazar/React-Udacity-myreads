3d97cadbfa51831240ec669b6655b3ebee693f2e
yaraElGazar <yara_elgazar@outlook.com>
2022-08-21 10:16:59 +0200

    add bookshelfs

--- Project/src/Main.js
+++ Project/src/Main.js
@@ -1,12 +1,33 @@
 import BookShelf from "./BookShelf";
+import { useEffect, useState } from "react";
+import * as BooksAPI from "./BooksAPI";
 
 const Main = () => {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const getBooks = async () => {
+      const response = await BooksAPI.getAll();
+      setBooks(response);
+    };
+    getBooks();
+  }, []);
   return (
     <div className='main'>
       <div className='list-books-title'>
         <h1>MyReads</h1>
       </div>
-      <BookShelf />
+      <BookShelf
+        title='Currently Reading'
+        books={books.filter((book) => book.shelf === "currentlyReading")}
+      />
+      <BookShelf
+        title='Want To Read'
+        books={books.filter((book) => book.shelf === "wantToRead")}
+      />
+      <BookShelf
+        title='Read'
+        books={books.filter((book) => book.shelf === "read")}
+      />
     </div>
   );
 };
