8a6660f53e945dc24c52c4c64941923a5171cf54
yaraElGazar <yara_elgazar@outlook.com>
2022-08-22 21:32:40 +0200

    Add getBooks and shelfChange functions

--- Project/src/App.js
+++ Project/src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import * as BooksAPI from "./BooksAPI";
 import Main from "./Main";
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  // Fetch all books from BooksAPI when loading
+  const getBooks = async () => {
+    const response = await BooksAPI.getAll();
+    setBooks(response);
+  };
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  // Updating books API then fetching books again
+  const shelfChange = async (book, shelf) => {
+    await BooksAPI.update(book, shelf);
+    await getBooks();
+  };
+
   return (
     <div className='app'>
-      <Main />
+      <Main books={books} shelfChange={shelfChange} />
     </div>
   );
 }
